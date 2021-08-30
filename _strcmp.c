21f3a5d681b504be5f1d74f7df2d1694927bcbe7
jrichard1909 <jrichard1909@gmail.com>
2021-08-30 15:25:27 -0700

    _strcmp.c file fixed for PATH

--- _strcmp.c
+++ _strcmp.c
@@ -9,16 +9,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int res = 0;
+	int i;
 
-	while (*s1 == *s2 && *s1 != '\0')
+	for (i = 0; s1[i]; i++)
 	{
-		s1++;
-		s2++;
+		if (s1[i] != s2[i])
+		return (1);
 	}
 
-	if (s1 != s2)
-		res = *s1 - *s2;
-
-	return (res);
+	return (0);
 }
